1d9ecc02cf2d06659d4608ed1b5dacca5189096e
zyich <zuilinho@yandex.ru>
2023-06-13 16:48:58 +0200

    first task sqaure of number

---
+++ Task1_square_of_Number/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Input number =>");
+int number  = Convert.ToInt32(Console.ReadLine());
+ // метод Конвертаци и тип 32 (кол-во символов в строке )
+
+int power =  2; // степень числа
+int squareNumber = (int)Math.Pow(number, power);
+// можно решать проще : int squareNumber = number * number; 
+
+Console.WriteLine($"Square {number} is {squareNumber}");
+//$ используют для вывода дополнительного текста
