79017710106bddcc201912e65f3ee9e706b13490
zyich <zuilinho@yandex.ru>
2023-06-15 12:45:33 +0200

    task 3 day of week qw
    ''
    '

---
+++ task3_DayOfWeek/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача №3. Напишите программу, которая
+ будет выдавать название дня недели 
+ по заданному номеру.
+3 -> Среда
+5 -> Пятница
+*/
+
+Console.WriteLine("vveditr chislo: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Dictionary<int, string> map = new Dictionary<int, string>()
+{
+    {1, "Ponedelnik"},
+    {2, "Vtornik"},
+    {3, "Sreda"},
+    {4, "Chetverg"},
+    {5, "Pyatnica"},
+    {6, "Subbota"},
+    {7, "Voskr"},
+};
+if (n > 0 && n < 8)
+{
+    Console.WriteLine(map[n]);
+}
+else
+{
+    Console.WriteLine($"Oshibka!");
+}
+
+//дикшанари это словарь в который помщаем массив с инт и стринг(ключ и значение) 
+// мап название словаря 
